52e21c42041dde552f71623f7b735c2e45a94369
pflis <piotrek610@wp.pl>
2021-07-28 16:05:40 +0200

    zadanie35

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let liczba_n = 99;
+if (liczba_n < 100) {
+console.log('Liczba nie znajduje się w przedziale');
+}   else if (liczba_n > 200) {
+    console.log('Liczba nie znajduje się w przedziale');
+}   else {
+    console.log('Liczba znajduje się w przedziale');
+    }
