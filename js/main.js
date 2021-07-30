9ae07fd8673cd6ecd8c6ff56c46c5b47ce2340a3
pflis610 <piotrek610@wp.pl>
2021-07-30 20:36:57 +0200

    zadanie35 korekta

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-let liczba_n = 99;
-if (liczba_n < 100) {
-console.log('Liczba nie znajduje się w przedziale');
-}   else if (liczba_n > 200) {
-    console.log('Liczba nie znajduje się w przedziale');
+let number_n = 99;
+let intervalcheck = function (number_n) {
+if (number_n < 100) {
+return 'Liczba nie znajduje się w przedziale';
+}   else if (number_n > 200) {
+    return 'Liczba nie znajduje się w przedziale';
 }   else {
-    console.log('Liczba znajduje się w przedziale');
+    return 'Liczba znajduje się w przedziale';
     }
+}
+console.log(intervalcheck(number_n));
