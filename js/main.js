a34e055d878753eec5548650bce4a0493f804cc6
pflis610 <piotrek610@wp.pl>
2021-08-02 14:59:27 +0200

    zadanie35 dopieszczenie

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let number_n = 99;
-let intervalcheck = function (number_n) {
-if (number_n < 100) {
-return 'Liczba nie znajduje się w przedziale';
-}   else if (number_n > 200) {
+let intervalcheck = function (num_n) {
+    if (num_n < 100) {
     return 'Liczba nie znajduje się w przedziale';
-}   else {
-    return 'Liczba znajduje się w przedziale';
-    }
+    }   else if (num_n > 200) {
+        return 'Liczba nie znajduje się w przedziale';
+    }   else {
+        return 'Liczba znajduje się w przedziale';
+        }
 }
 console.log(intervalcheck(number_n));
